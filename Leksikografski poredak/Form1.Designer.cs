0be2f445a3fd7bebab5bd63a234746dae2c943f1
tonipolanec <48983791+tonipolanec@users.noreply.github.com>
2019-12-29 17:56:56 +0100

    Add files via upload

---
+++ Leksikografski poredak/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Leksikografski_poredak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUnosSlova = new System.Windows.Forms.TextBox();
+            this.listBoxIspisPermutacija = new System.Windows.Forms.ListBox();
+            this.buttonPermutiraj = new System.Windows.Forms.Button();
+            this.radioButtonBrojevi = new System.Windows.Forms.RadioButton();
+            this.radioButtonSlova = new System.Windows.Forms.RadioButton();
+            this.labelUkupnoPermutacija = new System.Windows.Forms.Label();
+            this.checkBoxPronadiPermutaciju = new System.Windows.Forms.CheckBox();
+            this.labelPermutacijaPoRedu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxUnosSlova
+            // 
+            this.textBoxUnosSlova.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.textBoxUnosSlova.Location = new System.Drawing.Point(50, 13);
+            this.textBoxUnosSlova.Name = "textBoxUnosSlova";
+            this.textBoxUnosSlova.Size = new System.Drawing.Size(229, 38);
+            this.textBoxUnosSlova.TabIndex = 0;
+            // 
+            // listBoxIspisPermutacija
+            // 
+            this.listBoxIspisPermutacija.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.listBoxIspisPermutacija.FormattingEnabled = true;
+            this.listBoxIspisPermutacija.ItemHeight = 31;
+            this.listBoxIspisPermutacija.Location = new System.Drawing.Point(50, 206);
+            this.listBoxIspisPermutacija.Name = "listBoxIspisPermutacija";
+            this.listBoxIspisPermutacija.Size = new System.Drawing.Size(229, 314);
+            this.listBoxIspisPermutacija.TabIndex = 1;
+            this.listBoxIspisPermutacija.SelectedIndexChanged += new System.EventHandler(this.listBoxIspisPermutacija_SelectedIndexChanged);
+            // 
+            // buttonPermutiraj
+            // 
+            this.buttonPermutiraj.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.buttonPermutiraj.Location = new System.Drawing.Point(153, 69);
+            this.buttonPermutiraj.Name = "buttonPermutiraj";
+            this.buttonPermutiraj.Size = new System.Drawing.Size(126, 38);
+            this.buttonPermutiraj.TabIndex = 2;
+            this.buttonPermutiraj.Text = "Permutiraj!";
+            this.buttonPermutiraj.UseVisualStyleBackColor = true;
+            this.buttonPermutiraj.Click += new System.EventHandler(this.buttonPermutiraj_Click);
+            // 
+            // radioButtonBrojevi
+            // 
+            this.radioButtonBrojevi.AutoSize = true;
+            this.radioButtonBrojevi.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.radioButtonBrojevi.Location = new System.Drawing.Point(50, 57);
+            this.radioButtonBrojevi.Name = "radioButtonBrojevi";
+            this.radioButtonBrojevi.Size = new System.Drawing.Size(97, 30);
+            this.radioButtonBrojevi.TabIndex = 3;
+            this.radioButtonBrojevi.Text = "Brojevi";
+            this.radioButtonBrojevi.UseVisualStyleBackColor = true;
+            this.radioButtonBrojevi.CheckedChanged += new System.EventHandler(this.radioButtonBrojevi_CheckedChanged);
+            // 
+            // radioButtonSlova
+            // 
+            this.radioButtonSlova.AutoSize = true;
+            this.radioButtonSlova.Checked = true;
+            this.radioButtonSlova.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.radioButtonSlova.Location = new System.Drawing.Point(50, 86);
+            this.radioButtonSlova.Name = "radioButtonSlova";
+            this.radioButtonSlova.Size = new System.Drawing.Size(85, 30);
+            this.radioButtonSlova.TabIndex = 4;
+            this.radioButtonSlova.TabStop = true;
+            this.radioButtonSlova.Text = "Slova";
+            this.radioButtonSlova.UseVisualStyleBackColor = true;
+            // 
+            // labelUkupnoPermutacija
+            // 
+            this.labelUkupnoPermutacija.AutoSize = true;
+            this.labelUkupnoPermutacija.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.labelUkupnoPermutacija.Location = new System.Drawing.Point(45, 164);
+            this.labelUkupnoPermutacija.Name = "labelUkupnoPermutacija";
+            this.labelUkupnoPermutacija.Size = new System.Drawing.Size(0, 26);
+            this.labelUkupnoPermutacija.TabIndex = 5;
+            // 
+            // checkBoxPronadiPermutaciju
+            // 
+            this.checkBoxPronadiPermutaciju.AutoSize = true;
+            this.checkBoxPronadiPermutaciju.Checked = true;
+            this.checkBoxPronadiPermutaciju.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxPronadiPermutaciju.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.checkBoxPronadiPermutaciju.Location = new System.Drawing.Point(50, 122);
+            this.checkBoxPronadiPermutaciju.Name = "checkBoxPronadiPermutaciju";
+            this.checkBoxPronadiPermutaciju.Size = new System.Drawing.Size(225, 30);
+            this.checkBoxPronadiPermutaciju.TabIndex = 9;
+            this.checkBoxPronadiPermutaciju.Text = "Pronadi permutaciju";
+            this.checkBoxPronadiPermutaciju.UseVisualStyleBackColor = true;
+            this.checkBoxPronadiPermutaciju.CheckedChanged += new System.EventHandler(this.checkBoxPronadiPermutaciju_CheckedChanged);
+            // 
+            // labelPermutacijaPoRedu
+            // 
+            this.labelPermutacijaPoRedu.AutoSize = true;
+            this.labelPermutacijaPoRedu.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.labelPermutacijaPoRedu.Location = new System.Drawing.Point(45, 530);
+            this.labelPermutacijaPoRedu.MaximumSize = new System.Drawing.Size(240, 0);
+            this.labelPermutacijaPoRedu.Name = "labelPermutacijaPoRedu";
+            this.labelPermutacijaPoRedu.Size = new System.Drawing.Size(0, 26);
+            this.labelPermutacijaPoRedu.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 602);
+            this.Controls.Add(this.labelPermutacijaPoRedu);
+            this.Controls.Add(this.checkBoxPronadiPermutaciju);
+            this.Controls.Add(this.labelUkupnoPermutacija);
+            this.Controls.Add(this.radioButtonSlova);
+            this.Controls.Add(this.radioButtonBrojevi);
+            this.Controls.Add(this.buttonPermutiraj);
+            this.Controls.Add(this.listBoxIspisPermutacija);
+            this.Controls.Add(this.textBoxUnosSlova);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUnosSlova;
+        private System.Windows.Forms.ListBox listBoxIspisPermutacija;
+        private System.Windows.Forms.Button buttonPermutiraj;
+        private System.Windows.Forms.RadioButton radioButtonBrojevi;
+        private System.Windows.Forms.RadioButton radioButtonSlova;
+        private System.Windows.Forms.Label labelUkupnoPermutacija;
+        private System.Windows.Forms.CheckBox checkBoxPronadiPermutaciju;
+        private System.Windows.Forms.Label labelPermutacijaPoRedu;
+    }
+}
+
---
+++ Leksikografski poredak/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Leksikografski_poredak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Leksikografski poredak";
+            this.Icon = Properties.Resources._66116;
+        }
+
+        private void buttonPermutiraj_Click(object sender, EventArgs e)
+        {
+            listBoxIspisPermutacija.Items.Clear();
+
+            if (radioButtonBrojevi.Checked)
+            {
+
+                String brojceki = textBoxUnosSlova.Text;
+                int nBrojeva = 1;
+                for (int i = 0; i < brojceki.Length; i++)
+                {
+                    if (brojceki[i] == ',')
+                        nBrojeva++;
+                }
+                int[] brojevi = uzmiBrojeveIzStringa(brojceki, nBrojeva);
+                brojevi = sort(brojevi);
+
+                int nPermutacijaBr = faktorijel(nBrojeva);
+                int k = nadiDjeliteljZbogPonavljanjaElemenata(brojevi);
+                nPermutacijaBr = nPermutacijaBr / k;
+
+                labelUkupnoPermutacija.Text = "Ukupno ima " + nPermutacijaBr + " permutacija.";
+
+                listBoxIspisPermutacija.Items.Add("" + intArrToString(brojevi));
+
+                int[] permuteraniBrojevi = permuteraj(brojevi);
+                for (int i = 1; i < nPermutacijaBr; i++)
+                {
+                    listBoxIspisPermutacija.Items.Add("" + intArrToString(permuteraniBrojevi));
+                    permuteraniBrojevi = permuteraj(permuteraniBrojevi);
+                }
+
+            }
+            else if (radioButtonSlova.Checked)
+            {
+
+                String rijec = textBoxUnosSlova.Text;
+                int brojSlova = rijec.Length;
+                int nPermutacijaSl = faktorijel(brojSlova);
+
+                int k = nadiDjeliteljZbogPonavljanjaElemenata(pretvorba(rijec));
+                nPermutacijaSl /= k;
+
+                labelUkupnoPermutacija.Text = "Ukupno ima " + nPermutacijaSl + " permutacija.";
+
+                int[] intRijec = pretvorba(rijec);
+                intRijec = sort(intRijec);
+
+                listBoxIspisPermutacija.Items.Add("" + uzmiStringIzBrojeva(intRijec));
+
+                int[] permuteranaSlova = permuteraj(intRijec);
+                for (int p = 1; p < nPermutacijaSl; p++)
+                {
+                    listBoxIspisPermutacija.Items.Add("" + uzmiStringIzBrojeva(permuteranaSlova));
+
+                    permuteranaSlova = permuteraj(permuteranaSlova);
+                }
+
+                if (checkBoxPronadiPermutaciju.Checked)
+                {
+                    string[] listaPermutacija = listBoxIspisPermutacija.Items.OfType<string>().ToArray();
+                    int index = Array.IndexOf(listaPermutacija, rijec);
+                    labelPermutacijaPoRedu.Text = "Upisana kombinacija je " + (index+1) + ". po redu.";
+                }
+
+
+            }
+
+
+
+        }
+
+        public static int[] permuteraj(int[] arr)
+        {
+
+            int i = arr.Length - 1;
+
+            while (i > 0 && arr[i] <= arr[i - 1])
+                i--;
+
+            if (i <= 0)
+                return arr;
+
+
+            int j = arr.Length - 1;
+            while (arr[j] <= arr[i - 1])
+                j--;
+
+            //swap
+            int temp = arr[j];
+            arr[j] = arr[i - 1];
+            arr[i - 1] = temp;
+
+            //brni ga
+            j = arr.Length - 1;
+            while (i < j)
+            {
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+
+                i++;
+                j--;
+            }
+            return arr;
+
+        }
+
+        public static int[] pretvorba(String str)
+        {
+            int[] pretvoreno = new int[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                pretvoreno[i] = (int)str[i];
+            }
+
+            return pretvoreno;
+        }
+
+        public static int faktorijel(int n) {
+            int f = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                f *= i;
+            }
+
+            return f;
+        }
+
+        public static int[] uzmiBrojeveIzStringa(String str, int n)
+        {
+            int[] vratiTo = new int[n];
+
+            String trenutniBroj = "";
+            int indexBrojeva = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ',')
+                {
+                    trenutniBroj += str[i];
+                } else if (str[i] == ',')
+                {
+                    int broj = int.Parse(trenutniBroj);
+                    vratiTo[indexBrojeva] = broj;
+                    indexBrojeva++;
+                    trenutniBroj = "";
+                }
+                if (i == str.Length - 1)
+                {
+                    int broj = int.Parse(trenutniBroj);
+                    vratiTo[indexBrojeva] = broj;
+                    indexBrojeva++;
+                    trenutniBroj = "";
+                }
+            }
+            return vratiTo;
+        }
+
+        public static String uzmiStringIzBrojeva(int[] arr)
+        {
+            String str = "";
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                str += "" + (char)arr[i];
+            }
+            return str;
+        }
+
+        public static String intArrToString(int[] arr)
+        {
+            String str = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i != arr.Length - 1)
+                {
+                    str += "" + arr[i] + ", ";
+                }else{
+                    str += "" + arr[i];
+                }
+            }
+            return str;
+        }
+
+        public static int nadiDjeliteljZbogPonavljanjaElemenata(int[] arr)
+        {
+            int k = 1;
+
+            int[] ponavljani = new int[20];
+            int indeksP = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!Array.Exists(ponavljani, element => element == arr[i]))
+                {
+                    int trenutni = arr[i];
+                    ponavljani[indeksP] = trenutni; indeksP++;
+                    int tempK = 0;
+
+                    for (int j = 0; j < arr.Length; j++)
+                    {
+                        if (trenutni == arr[j])
+                        {
+                            tempK++;
+
+                        }
+                    }
+                    k *= faktorijel(tempK);
+                }
+            }
+            return k;
+        }
+
+        //OVO SE JOS MOZDA PREMENI
+        private void listBoxIspisPermutacija_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sel = listBoxIspisPermutacija.SelectedIndex + 1;
+            MessageBox.Show("Odabrana permutacija je " + sel + ". po redu");
+        }
+
+
+        public static int[] sort(int[] arr)
+        {
+            
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+
+        private void checkBoxPronadiPermutaciju_CheckedChanged(object sender, EventArgs e)
+        {
+            labelPermutacijaPoRedu.Text = "";
+        }
+
+        private void radioButtonBrojevi_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButtonBrojevi.Checked)
+                checkBoxPronadiPermutaciju.Enabled = false;
+            else
+                checkBoxPronadiPermutaciju.Enabled = true;
+
+        }
+    }
+
+
+}
